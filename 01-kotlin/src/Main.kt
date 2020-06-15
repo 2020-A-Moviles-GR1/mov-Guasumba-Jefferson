a68d6c0021e300e8e95273b704b323b1f53fd00e
Jefferson Guasumba <jefferson.guasumba@epn.edu.ec>
2020-06-15 09:00:26 -0500

    Array, Map, Filter

--- 01-kotlin/src/Main.kt
+++ 01-kotlin/src/Main.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.collections.ArrayList
 
 fun main(args:Array<String>){
 
@@ -53,11 +54,82 @@ fun main(args:Array<String>){
     calcularSueldo(1000.00, 13.00)
     calcularSueldo(
             tasa = 121.00,
-            sueldo = 121.12
+            sueldo = 121.12     
     ) // Named Parameters
     calcularSueldo(123.00)
     calcularSueldo(sueldo= 123.00)
 
+    val arregloConstante: Array<Int> = arrayOf(1,2,3)  //Arreglos(El Array no se va a poder modificar)
+    val arregloCumpleanos: ArrayList<Int> = arrayListOf(22,31,33,34,35) //Este arreglo puede modificarse
+    arregloCumpleanos.add(24)
+    arregloCumpleanos.remove(element = 30)
+    print(arregloCumpleanos)
+
+    //3 formas de rrecorrer un arreglo
+
+    arregloCumpleanos.forEach {  //Para rrecorrer el array
+
+        println("Valor de la iteracion" + it)
+    }
+
+    arregloCumpleanos.forEach { valorIteracion: Int ->
+
+    println("ValorIteracion" + valorIteracion)
+
+
+    }
+
+    //Operadores -> TODOS LOS LENGUAJES
+    //El foreach no devuelve nada -> unit
+    arregloCumpleanos.forEach( //Solo para iterar
+            {valorIteracion: Int ->
+                println("Valor iteracion" + valorIteracion)
+                println("valor con -1 = ${valorIteracion * -1}")
+
+            }
+
+    )
+
+    arregloCumpleanos.forEachIndexed { index:Int, it:Int -> //Para iterar con indice
+            println("Valor de la iteracion" + it + "    " + "El indice es:" + index)
+
+    }
+
+    //MAP
+    //1) Enviamos el nuevo valor de la iteracion
+    //2)Nos Devulve un nuevo arreglo con los valores modificados
+    //3) Nos devuelve un nuevo arreglo
+
+    val respuestaMap = arregloCumpleanos.map {iterador: Int -> //para modificar el arreglo
+
+        iterador * -1
+    }
+
+    val respuestaMapDos = arregloCumpleanos.map {iterador: Int -> //para modificar el arreglo
+
+        val  nuevoValor: Int = iterador * -1
+        val  otroValor: Int = nuevoValor * 2
+        return@map otroValor.toString() //no solo modifica el arregla si no que devuelve culquier cosa
+    }
+
+
+    println(respuestaMap)
+    println(respuestaMapDos)
+    println(arregloCumpleanos)
+
+
+    //Filter -> Filtra el arreglo, me devuelve un nuevo arreglo
+    //1)Devolver una expresion
+    //2)Nos devuleve un nuevo arreglo que cumpla esa expresion
+
+   val respuestaFilter = arregloCumpleanos.filter { iteracion: Int ->
+
+        val esMayorA23= iteracion > 23
+        return@filter esMayorA23
+
+    }
+
+    println(respuestaFilter)
 }
 
 //Funciones
@@ -65,7 +137,7 @@ fun main(args:Array<String>){
   fun calcularSueldo (sueldo: Double,   //Requeridos!
                       tasa: Double = 12.00, //(Tiene valor defecto)
                       calculoEspecial: Int? = null//opcional!! (Pueden ser nulos)
-  ): Double {
+  ): Double? { // el valor puede ser nulo
     if (calculoEspecial != null) {
         return sueldo * tasa * calculoEspecial
     }else{
@@ -79,3 +151,4 @@ fun main(args:Array<String>){
         println("")
 
     }
+
